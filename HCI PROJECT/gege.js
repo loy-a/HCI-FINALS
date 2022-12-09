21b5a782682aff95727bd8d2658cb449a8ee217d
Reymark Loy-a <72779379+loy-a@users.noreply.github.com>
2022-12-09 13:01:24 +0800

    Add files via upload

---
+++ HCI PROJECT/gege.js
@@ -0,0 +1,25 @@
+const loginArea = document.getElementById('login-area'),
+    dashboard = document.getElementById('dashboard'),
+    deposit = document.getElementById('deposit'),
+    withdraw = document.getElementById('withdraw'),
+    balance = document.getElementById('balance'),
+    depositInput = document.getElementById('deposit-input'),
+    withdrawInput = document.getElementById('withdraw-input'),
+    depositBtn = document.getElementById('deposit-btn'),
+    withdrawBtn = document.getElementById('withdraw-btn'),
+    submitBtn = document.getElementById('submit-btn');
+
+
+submitBtn.addEventListener('click', () => {
+    loginArea.style.display = 'none';
+    dashboard.classList.remove('d-none');
+})
+
+depositBtn.addEventListener('click', () => {
+    const value = depositInput.value;
+    const depositValue = Number(deposit.innerText) + Number(value);
+    const balanceValue = Number(balance.innerText) + Number(value);
+    deposit.innerText = depositValue;
+    balance.innerText = balanceValue;
+    depositInput.value = '';
+})
